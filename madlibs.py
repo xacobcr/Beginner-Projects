024235d550d52e497a0845e9a6b6920cf1c1caa0
Jacob <jacobcreed96@gmail.com>
2022-06-30 14:27:30 -0700

    Madlibs First commit

---
+++ madlibs.py
@@ -0,0 +1,24 @@
+# User inputs initalized to variables
+adj = input("Adjective: ")
+adj2 = input("Adjective: ")
+noun = input("Singular Noun: ")
+adj3 = input("Adjective: ")
+noun2 = input("Singular Noun: ")
+adj4 = input("Adjective: ")
+noun3 = input("Singular Noun: ")
+verb = input("Verb: ")
+noun4 = input("Singular Noun: ")
+verb2 = input("Verb: ")
+noun5 = input("Singular Noun: ")
+adj5 = input("Adjective: ")
+adj6 = input("Adjective: ")
+noun6 = input("Plural Noun: ")
+country = input("Country: ")
+noun7 = input("Singular Noun: ")
+animal = input("Animal: ")
+
+# madlib story with variables
+madlib = f"     My cubicle is {adj}. It is the {adj2} cubicle in the office. I have a {noun} on my desk next to a(n) {adj3} {noun2}. In my drawer, I also have a(n) {adj4} {noun3}. One time a coworker tried to {verb} a {noun4} on my desk.\n I said to him 'Hey! how would you like it if I {verb2} your {noun5}? Ill do it if you don't leave.'\n     My one complaint about my cubicle is that it is {adj5}. I think everyone here at the office complains about this. We also complain that our cubicle is {adj6}. If we had money in our budget, my boss could purchase some {noun6} to help alleviate this problem. Our boss doesn't understand. His office is the size of {country}. He has enough room in his office to put a {noun7} {animal} in there."
+
+# print madlib story
+print(madlib)
