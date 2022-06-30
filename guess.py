09e4df434abb588d92dc2fd5da4c0e07a44b6fee
Jacob <jacobcreed96@gmail.com>
2022-06-30 15:32:13 -0700

    Second commit. Adding next function, computer guess your number

--- guess.py
+++ guess.py
@@ -1,5 +1,7 @@
 import random
 
+# input a number to guess the computers random generated number
+
 
 def guess(x):
     random_number = random.randint(1, x)
@@ -14,4 +16,20 @@ def guess(x):
     print(f'Congrats!! You guessed the random number {x}!')
 
 
-guess(20)
+# input a high range for the computer to guess the random number youre thinking
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+        feedback = input(
+            f'Is {guess} too high(h), too low(L), or correct(C)? ').lower
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+    print(f'The computer has guessed your number, {guess}, correctly!')
